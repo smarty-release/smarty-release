835c5e42eb6f39a64f4424bdc1b58717a61a8d19
ajiho <lujiahao@88.com>
2026-04-18 01:41:15 +0800

    日志生成之后格式化日志
    Signed-off-by: ajiho <lujiahao@88.com>

--- smarty-release.config.ts
+++ smarty-release.config.ts
@@ -7,16 +7,12 @@ export default defineConfig({
     changelog: {
       args: "-o --tag ${version}",
       template: "github",
-      // 是用来覆盖上面的预设的
-      config: {
-        changelog: {
-          header: "a",
-        },
-      },
     },
     requireBranch: false,
     commitMessage: "release: v${version}",
     tagName: "v${version}",
   },
-  hooks: {},
+  hooks: {
+    "after:changelog": "prettier --write CHANGELOG.md",
+  },
 });
