261ac1baeea98c79f89ca44c5a5f364592467a5f
ajiho <lujiahao@88.com>
2026-04-18 01:21:17 +0800

    格式化文件
    Signed-off-by: ajiho <lujiahao@88.com>

--- smarty-release.config.ts
+++ smarty-release.config.ts
@@ -6,17 +6,9 @@ export default defineConfig({
   git: {
     changelog: {
       args: "-o --tag ${version}",
-      // 也可以是数组
-      // args: ["-o", "--tag", "v${version}"],
-      template: "cocogitto",
+      template: "github",
       // 是用来覆盖上面的预设的
       config: {
-        remote: {
-          github: {
-            owner: "ajiho",
-            repo: "releaset-it-demo",
-          },
-        },
         changelog: {
           header: "a",
         },
@@ -26,21 +18,5 @@ export default defineConfig({
     commitMessage: "release: v${version}",
     tagName: "v${version}",
   },
-  hooks: {
-    // 最后都应该转换成数组(数组元素支持字符串和函数)才合适
-    "before:init": ["npm run test"],
-    // "before:selectVersion": ({ logger }) => {
-    //   logger.info("Before selecting version");
-    // },
-    // "after:selectVersion": ({ version, logger }) => {
-    //   logger.info(`after selecting version:${version}`);
-    // },
-    // "after:bump": [
-    //   "npm run changelog",
-    //   async () => {
-    //     console.log("after:bump");
-    //   },
-    // ],
-    // "after:release": "echo 已推送 v${version} ",
-  },
+  hooks: {},
 });
