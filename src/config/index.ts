403cf72b64ed3fa4142fb1835f574923eeccf2c7
ajiho <lujiahao@88.com>
2026-04-23 22:35:12 +0800

    支持json配置文件优化配置加载
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,10 @@
 import { resolve } from "node:path";
 import { lilconfig } from "lilconfig";
-import type { Loader } from "lilconfig";
-import { pathExists } from "../utils/fs.ts";
-
+import type { Loader, LilconfigResult } from "lilconfig";
 export * from "./types.ts";
 
-const loadTs: Loader = async (filepath) => {
-  const { createJiti } = await import("jiti");
-  return createJiti(import.meta.url, { interopDefault: true }).import(
-    filepath,
-    { default: true },
-  );
-};
+type LoaderReturn = Promise<Awaited<ReturnType<Loader>>>;
+type LoaderFilepath = Parameters<Loader>[0];
 
 export async function loadConfig<T>(
   name: string,
@@ -19,29 +12,31 @@ export async function loadConfig<T>(
 ): Promise<T> {
   const explorer = lilconfig(name, {
     loaders: {
-      ".mts": loadTs,
-      ".ts": loadTs,
-      ".cts": loadTs,
+      ".mts": loadTS,
+      ".ts": loadTS,
+      ".cts": loadTS,
     },
     searchPlaces: [
       `${name}.config.ts`,
       `${name}.config.mts`,
       `${name}.config.cts`,
+      `${name}.config.json`,
     ],
   });
 
-  let result;
+  const result: LilconfigResult = configPath
+    ? await explorer.load(resolve(process.cwd(), configPath))
+    : await explorer.search();
 
-  if (configPath) {
-    const filepath = resolve(process.cwd(), configPath);
-
-    if (!(await pathExists(filepath))) {
-      throw new Error(`Config file not found: ${filepath}`);
-    }
-    result = await explorer.load(filepath);
-  } else {
-    result = await explorer.search();
+  if (!result) {
+    return {} as T;
   }
-
-  return (result?.config ?? {}) as T;
+  return result.config as T;
+}
+async function loadTS(filepath: LoaderFilepath): LoaderReturn {
+  const { createJiti } = await import("jiti");
+  return createJiti(import.meta.url, { interopDefault: true }).import(
+    filepath,
+    { default: true },
+  );
 }
